0aad89b330dc84942251e7cf4b7fec05e4eedc63
haha <haha>
2017-07-12 10:58:54 -0300

    turtlebot

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install ros-kinetic-turtlebot ros-kinetic-turtlebot-apps ros-kinetic-turtlebot-interactions ros-kinetic-turtlebot-simulator ros-kinetic-kobuki-ftdi ros-kinetic-ar-track-alvar-msgs
+export TURTLEBOT_GAZEBO_WORLD_FILE=/opt/ros/kinetic/share/turtlebot_gazebo/worlds/playground.world
+roslaunch turtlebot_gazebo turtlebot_world.launch
+
